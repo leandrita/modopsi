5aa6ff6b1c560c7243a8a2d4e8a06b5ba2afbc63
leandrita <lea.bujh@gmail.com>
2024-01-19 12:44:02 +0100

    create services directory with apiservices file and install axios

---
+++ client/src/services/ApiServices.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const endpoint = 'http://localhost:8000/api';
+
+export async function getAllTherapies() {
+    const response = await axios.get(`${endpoint}/therapies`);
+    return response.data;
+}
+
+export async function getTherapyById(id) {
+    try {
+        const response = await axios.get(`${endpoint}/therapy/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function getAllProfessionals() {
+    const response = await axios.get(`${endpoint}/professionals`);
+    return response.data;
+}
+
+export async function getProfessionalById(id) {
+    try {
+        const response = await axios.get(`${endpoint}/professional/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
