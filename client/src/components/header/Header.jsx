6e553d11fa35ae0b315e0953de36daf5ffda9b02
leandrita <lea.bujh@gmail.com>
2024-02-07 11:19:50 +0100

    create menu burguer for small screens

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import './Header.css'
 import Logo3 from '../../assets/Logo3.svg'
 import Nav from '../../components/nav/Nav'
+import Menu from '../menu/Menu'
 
 const Header = ({ currentPage }) => {
+    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLargeScreen(window.innerWidth >= 1024);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const isHomePage = currentPage === '/';
+    const isSmallScreen = !isLargeScreen;
+    const isHomePageClass = isHomePage ? 'home-page' : '';
+    const menuIcon = isHomePage ? 'MenuCream.svg' : 'MenuGreen.svg';
 
     return (
-        <div className={`header ${isHomePage ? 'home-page' : ''}`}>
-            {!isHomePage && <img className='small-logo' src={Logo3} alt="Logo" />}
-            <Nav isHomePage={isHomePage} />
+        <div className={`header ${isHomePageClass}`}>
+            {isSmallScreen ? (
+                isHomePage ? <Menu currentPage={currentPage} icon={menuIcon} /> : (
+                    <>
+                        <img className='small-logo' src={Logo3} alt="Logo" />
+                        <Menu currentPage={currentPage} icon={menuIcon} />
+                    </>
+                )
+            ) : (
+                <>
+                    {isHomePage ? <Nav isHomePage={true} /> : (
+                        <>
+                            <img className='small-logo' src={Logo3} alt="Logo" />
+                            <Nav isHomePage={false} />
+                        </>
+                    )}
+                </>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Header
---
+++ client/src/components/menu/Menu.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import './Menu.css';
+import { Link } from "react-router-dom";
+import CruzGreen from '../../assets/CruzGreen.svg';
+import MenuCream from '../../assets/MenuCream.svg';
+import MenuGreen from '../../assets/MenuGreen.svg';
+
+const Menu = ({ icon }) => {
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
+    const menuIcon = icon === 'MenuCream.svg' ? MenuCream : MenuGreen;
+
+    return (
+        <div className="menu-container">
+            {menuVisible && <div className="overlay" onClick={toggleMenu}></div>}
+            <div className="menu-hamburguesa">
+                <div className="menu-icon" onClick={toggleMenu}>
+                    <img src={menuIcon} alt="" />
+                </div>
+                {menuVisible && (
+                    <div>
+                        <div className="close-icon" onClick={toggleMenu}>
+                            <span><img src={CruzGreen} alt="" /></span>
+                        </div>
+                        <ul className="menu-list">
+                            <li className="list-item">
+                                <Link to={'/'} className='menu-link'>
+                                    <p>Quienes somos?</p>
+                                </Link>
+                            </li>
+                            <li className="list-item">
+                                <Link to={'/therapies'} className='menu-link'>
+                                    <p>Terapias</p>
+                                </Link>
+                            </li>
+                            <li className="list-item">
+                                <Link to={'/team'} className='menu-link'>
+                                    <p>Equipo</p>
+                                </Link>
+                            </li>
+                            <li className="list-item">
+                                <Link to={'/contact'} className='menu-link'>
+                                    <p>Contacto</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
